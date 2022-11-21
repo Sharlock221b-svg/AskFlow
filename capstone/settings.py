926818ed92901b4aa4a8dcb6d41c6a12871a5542
Dhairya Upadhyay <dhairya221b@gmail.com>
2022-11-21 18:59:20 +0530

    try

--- capstone/settings.py
+++ capstone/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@^881ql54n2&f9p(gqrw&1a&5nuh@+l*(o1i53n5e#tujlc=ky'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
